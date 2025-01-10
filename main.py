c4486d90c67d68bcb82296c05983f7f435eaa42c
Nicolas Guarini <nikiguarincia@gmail.com>
2025-01-10 18:26:42 +0100

    Create controlled and, or, xor gates

--- main.py
+++ main.py
@@ -17,6 +17,20 @@ def xor_gate(circuit, a, b, output):
     circuit.cx(a, output)
     circuit.cx(b, output)
 
+def controlled_and_gate(circuit, c, a, b, output):
+    circuit.mcx([c, a, b], output)
+
+
+def controlled_or_gate(circuit, c, a, b, output):
+    circuit.mcx([c, a], output)
+    circuit.mcx([c, b], output)
+    circuit.mcx([c, a, b], output)
+
+
+def controlled_xor_gate(circuit, c, a, b, output):
+    circuit.mcx([c, a], output)
+    circuit.mcx([c, b], output)
+
 
 def reset_bits(bits):
     for i in bits:
