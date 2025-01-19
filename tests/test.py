dc85ca0d9d4a3b48f1e9b6ecdb16f5d05e700019
Noemie MANSOUR <noemie.mansour@edu.ece.fr>
2025-01-19 18:09:59 +0100

    tests: Add test for set_bits and copy

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,12 @@
 import unittest
 from qiskit import QuantumCircuit, transpile
 from qiskit_aer import AerSimulator
+import sys
+import os
 
-from src.main import *
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from main import *
 
 backend = AerSimulator()
 
@@ -87,6 +91,52 @@ class Test(unittest.TestCase):
         counts = result_sim.get_counts(transpiled_circuit)
         
         self.assertEqual(counts, expected_counts)
+    
+    def test_set_bits(self):
+        tests = [
+            ["0101", [0, 1, 2, 3]],
+            ["1010", [0, 1, 2, 3]],
+            ["1100", [0, 1, 2, 3]],
+            ["0011", [0, 1, 2, 3]],
+        ]
 
+        for X, A in tests:
+            circuit = QuantumCircuit(4, 4)
+            set_bits(circuit, A, X="".join(reversed(X)))
+            circuit.measure(A, A)
+
+            transpiled_circuit = transpile(circuit, backend)
+            n_shots = 1024
+            job_sim = backend.run(transpiled_circuit, shots=n_shots)
+            result_sim = job_sim.result()
+            counts = result_sim.get_counts(transpiled_circuit)
+
+            expected_counts = {X: n_shots}
+            self.assertEqual(counts, expected_counts)
+
+    def test_copy(self):
+        tests = [
+            ["0101", [0, 1, 2, 3], [4, 5, 6, 7]],
+            ["1010", [0, 1, 2, 3], [4, 5, 6, 7]],
+            ["1100", [0, 1, 2, 3], [4, 5, 6, 7]],
+            ["0011", [0, 1, 2, 3], [4, 5, 6, 7]],
+        ]
+
+        for X, A, B in tests:
+            circuit = QuantumCircuit(8, len(X))
+            set_bits(circuit, A, X="".join(reversed(X)))
+            copy(circuit, A, B)
+            circuit.measure(B, range(len(X)))
+
+            transpiled_circuit = transpile(circuit, backend)
+            n_shots = 1024
+            job_sim = backend.run(transpiled_circuit, shots=n_shots)
+            result_sim = job_sim.result()
+            counts = result_sim.get_counts(transpiled_circuit)
+
+            expected_counts = {X: n_shots}
+            self.assertEqual(counts, expected_counts)
+    
+            
 if __name__ == '__main__':
     unittest.main()
